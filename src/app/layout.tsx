d6080439c542d286d1cfb28c43ed81c45b293a8e
SANDER BOSSUYT <sander.bossuyt@mercedes-benz.com>
2025-12-15 12:01:16 +0100

    change meta setting for white bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Claimb" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </head>
 
       {/* 
